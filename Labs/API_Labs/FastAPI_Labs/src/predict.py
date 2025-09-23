0615fe472132087f9072d84ee4b0aa57763522f9
Atharv Talnikar <talnikaratharv@gmail.com>
2025-09-22 20:13:43 -0400

    Added two new classifiers

--- Labs/API_Labs/FastAPI_Labs/src/predict.py
+++ Labs/API_Labs/FastAPI_Labs/src/predict.py
@@ -1,13 +1,20 @@
 import joblib
 
-def predict_data(X):
+def predict_data(X, model_type="decision_tree"):
     """
-    Predict the class labels for the input data.
+    Predict the class labels for the input data using the specified model.
     Args:
         X (numpy.ndarray): Input data for which predictions are to be made.
+        model_type (str): 'decision_tree', 'random_forest', or 'logistic_regression'.
     Returns:
         y_pred (numpy.ndarray): Predicted class labels.
     """
-    model = joblib.load("../model/iris_model.pkl")
+    if model_type == "random_forest":
+        model_path = "../model/iris_model_randomforest.pkl"
+    elif model_type == "logistic_regression":
+        model_path = "../model/iris_model_logreg.pkl"
+    else:
+        model_path = "../model/iris_model.pkl"
+    model = joblib.load(model_path)
     y_pred = model.predict(X)
     return y_pred
